9ecbaf90aad83a3110ad10a833d06a1230880e6f
anasvakyathodi <avtvld@gmail.com>
2020-04-18 15:48:41 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-$("img").click(() => {
-  number = Math.floor(Math.random() * 6) + 1;
-  $("img").delay(500).fadeIn();
-  $("img").attr("src", `./img/${number}.png`);
+$(document).ready(function () {
+  alert("Page is ready");
+  $("img").click(() => {
+    number = Math.floor(Math.random() * 6) + 1;
+    $("img").delay(500).fadeIn();
+    $("img").attr("src", `./img/${number}.png`);
+  });
 });
