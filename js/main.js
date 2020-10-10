1c468d28a1920312a1219da755cdf8edbe6838a4
GShacklebolt <ghazali123456@gmail.com>
2020-10-10 20:03:17 +0530

    Added dice-rolling animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
   alert("Page is ready");
   $("img").click(() => {
-    number = Math.floor(Math.random() * 6) + 1;
-    $("img").delay(500).fadeIn();
-    $("img").attr("src", `./img/${number}.png`);
+  	var counter=0;
+  	animator=window.setInterval(function(){
+		counter++;
+		number = Math.floor(Math.random() * 6) + 1;
+    	$("img").attr("src", `./img/${number}.png`);
+    	if(counter==10){
+    		window.clearInterval(animator);
+    	}
+	},100);
   });
 });
